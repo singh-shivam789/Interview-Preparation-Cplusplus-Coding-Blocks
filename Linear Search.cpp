03f232b75249c581c99e056e60da3dd8205f2162
Shivam Singh <52860981+singh-shivam789@users.noreply.github.com>
2020-11-12 22:58:07 +0530

    Update Linear Search.cpp

--- Linear Search.cpp
+++ Linear Search.cpp
@@ -1,15 +1,22 @@
+///Linear Search
+
+///Search a particular element in the array
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
     int n,key;
-    int arr[1000] = {0};
     cin>>n;
+
+    int arr[1000] = {0};
     for(int i=0; i<n; i++)
     {
         cin>>arr[i];
     }
-    cout<<"Enter the key you want to search ";
+
+    ///Ask for the element we want to search
+    cout<<"Enter the element you want to search: ";
     cin>>key;
 
     ///Linear Search Algorithm
@@ -18,13 +25,13 @@ int main()
     {
         if(arr[i] == key)
         {
-            cout<<"The key is found at index "<<i<<endl;
+            cout<<key<<" is found at index: "<<i<<endl;
             break;
         }
     }
     if(i == n)
     {
-        cout<<"The key wasn't found in the array!"<<endl;
+        cout<<key<<" wasn't found in the array!"<<endl;
     }
     return 0;
 }
